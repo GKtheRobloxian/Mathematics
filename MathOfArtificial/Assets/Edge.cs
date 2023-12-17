bc447329c9901fc5c03c821815090abd474526e1
Samuel Lanzuela <amlanzuela@gmail.com>
2023-12-16 18:36:19 -0800

    Not too much progress for now

---
+++ MathOfArtificial/Assets/Edge.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Edge
+{
+
+    public Node startNode;
+    public Node endNode;
+
+    public Edge (Node from, Node to)
+    {
+        startNode = from;
+        endNode = to;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
