4e1851d785621b4b3cf1fc8690745e44571f632b
Samuel Lanzuela <amlanzuela@gmail.com>
2023-12-19 17:39:25 -0800

    This tutorial man

--- MathOfArtificial/Assets/Graph.cs
+++ MathOfArtificial/Assets/Graph.cs
@@ -6,7 +6,7 @@ public class Graph
 {
     List<Edge> edges = new List<Edge>();
     List<Node> nodes = new List<Node>();
-    List<Node> pathList = new List<Node>();
+    public List<Node> pathList = new List<Node>();
 
     public Graph()
     {
@@ -40,8 +40,91 @@ public class Graph
             {
                 return n;
             }
-            return null;
         }
+        return null;
+    }
+
+    public bool AStar(GameObject startId, GameObject endId)
+    {
+        Node start = FindNode(startId);
+        Node end = FindNode(endId);
+
+        if (start == null || end == null)
+        {
+            return false;
+        }
+
+        List<Node> open = new List<Node>();
+        List<Node> closed = new List<Node>();
+        float tentativeGScore = 0;
+        bool tentativeIsBetter;
+
+        start.g = 0;
+        start.h = distance(start, end);
+        start.f = start.h;
+
+        open.Add(start);
+        while(open.Count > 0)
+        {
+            int i = lowestF(open);
+            Node thisNode = open[i];
+            if (thisNode.getId() == endId)
+            {
+                //ReconstructPath(start, end);
+                return true;
+            }
+
+            open.RemoveAt(i);
+            closed.Add(thisNode);
+            Node neighbour;
+            foreach(Edge e in thisNode.edgeList)
+            {
+                neighbour = e.endNode;
+
+                if (closed.IndexOf(neighbour) > -1)
+                {
+                    continue;
+                }
+
+                tentativeGScore = thisNode.g + distance(thisNode, neighbour);
+                if (open.IndexOf(neighbour) == -1)
+                {
+                    open.Add(neighbour);
+                    tentativeIsBetter = true;
+                }
+                else if (tentativeGScore < neighbour.g)
+                {
+                    tentativeIsBetter = true;
+                }
+                else
+                {
+                    tentativeIsBetter = false;
+                }
+
+                if (tentativeIsBetter)
+                {
+                    neighbour.cameFrom = thisNode;
+                    neighbour.g = tentativeGScore;
+                    neighbour.h = distance(thisNode, end);
+                    neighbour.f = neighbour.g + neighbour.h;
+                }
+            }
+        }
+        return false;
+    }
+
+    public void ReconstructPath(Node startId, Node endId)
+    {
+        pathList.Clear();
+        pathList.Add(endId);
+        
+        var p = endId.cameFrom;
+        while(p != startId && p != null)
+        {
+            pathList.Insert(0, p);
+            p = p.cameFrom;
+        }
+        pathList.Insert(0, startId);
     }
 
     float distance(Node a, Node b)
@@ -55,6 +138,17 @@ public class Graph
         int count = 0;
         int iteratorCount = 0;
         lowestf = l[0].f;
+
+        for (int i = 1; i < l.Count; i++)
+        {
+            if (l[i].f <= lowestf)
+            {
+                lowestf = l[i].f;
+                iteratorCount = count;
+            }
+            count++;
+        }
+        return iteratorCount;
     }
 
     // Start is called before the first frame update
